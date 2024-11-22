ceb594ea6aa763d22fdacb9d12a417fe82e1c787
Lorenzo <lorenzocampagna61@gmail.com>
2024-11-22 14:12:45 +0100

    progettoOuyy

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.mycompany.progettogruppo10 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.progettogruppo10 to javafx.fxml;
+    exports com.mycompany.progettogruppo10;
+}
+*/
