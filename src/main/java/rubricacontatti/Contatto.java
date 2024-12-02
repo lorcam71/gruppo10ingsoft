ed02207ef6cd00538d4cef443e99c9f356b37aaa
ferra <ferra@LAPTOP-N8GU0T7N>
2024-12-02 17:17:58 +0000

    Aggiunta descrizione dox contatto

--- src/main/java/rubricacontatti/Contatto.java
+++ src/main/java/rubricacontatti/Contatto.java
@@ -8,9 +8,19 @@ package rubricacontatti;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author lorenzo
+/** @file Contatto.java
+ * 
+ * 
+ * 
+ *  @brief Il file contiene l'implementazione di un contatto 
+ * 
+ *  Contatto estende Persona e gestisce numeri ed email di un contatto in degli arrayList.
+ *  Utilizza le funzioni addNumero addEmail removeNumero removeEmail 
+ * 
+ * 
+ *  @author Gruppo10
+ *  @date December 1, 2024
+ *  @version 1.0
  */
 public class Contatto extends Persona{
     
@@ -20,23 +30,45 @@ public class Contatto extends Persona{
     public Contatto(String nome, String cognome, String numero1, String numero2,
         String numero3, String email1, String email2, String email3) {
         super(nome,cognome);
-        
+        numeri = new ArrayList<String>();
+        email = new ArrayList<String>();
+
         
     }
-    
+    /**
+     * @brief Aggiunge un numero al contatto
+     * 
+     * @param[in] numero da aggiungere al contatto
+     * 
+    */
     public void addNumero(String numero){
         
     }
-    
-    public void addEmail(String numero){
+    /**
+     * @brief Aggiunge una mail 
+     * 
+     * @param[in] email email da aggiungere al contatto
+     * 
+    */
+    public void addEmail(String email){
         
     }
-    
+    /**
+     * @brief rimuove un numero passato come argomento 
+     * 
+     * @param[in] numero numero da aggiungere al contatto
+     * 
+    */
     public void removeNumero(String numero){
         
     }
-    
-    public void removeEmail(String numero){
+    /**
+     * @brief Aggiunge una mail 
+     * 
+     * @param[in] email email da aggiungere al contatto
+     * 
+    */
+    public void removeEmail(String email){
         
     }
 
