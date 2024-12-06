26e1bf1b73aadf91d5e0f78d1088d9dcee90cbe5
Ciccone Emanuele <emanuele@localhost>
2024-12-06 11:09:44 +0100

    Aggiunta classe improntata all'importazione e esportazione

---
+++ src/main/java/rubricacontatti/ImportExport.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubricacontatti;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author emanuele
+ */
+public class ImportExport {
+    
+    
+    
+    /**
+     * @brief Esegue l'importazione della rubrica
+     * 
+     * Carica nell'arrayList i contatti da un file
+     * 
+     * @param[in] filename indica il file da cui eseguire l'importazione 
+     * 
+     * 
+     * @return contiene una lista di contatti imporatati dal file
+     * 
+    */
+    
+    public ArrayList<Contatto> importRubrica(String fileName){
+        
+    }
+    
+    
+    
+    /**
+     * @brief Esegue l'esportazione della rubrica 
+     * 
+     * Esporta i contatti dell'arrayList in un file esterno specificato dall'utente
+     * 
+     * @param[in] filename indica il file su cui esportare la rubrica
+     * @param[in] rubruca indica la lista di contatti che verra esportata
+     * 
+    */
+    
+    public void exportRubrica(String fileName, ArrayList<Contatto> rubrica){
+        
+    }
+    
+}
--- src/main/java/rubricacontatti/Rubrica.java
+++ src/main/java/rubricacontatti/Rubrica.java
@@ -20,9 +20,11 @@ import java.util.ArrayList;
  */
 public class Rubrica {
     private ArrayList<Contatto> rubrica;
+    private ImportExport importExport;
     
     public Rubrica(){
         rubrica = new ArrayList<Contatto>();
+        importExport = new ImportExport();
     }
     /**
      * @brief la funzione aggiunge un contatto alla lista 
@@ -52,34 +54,8 @@ public class Rubrica {
      * 
      * 
     */
-    public Contatto cercaContatto(String sottostringa){
+    public Contatto searchContatto(String sottostringa){
         
-    }
-
-    /**
-     * @brief Esegue l'importazione della rubrica
-     * 
-     * Carica nell'arrayList i contatti da un file
-     * 
-     * @param[in] filename indica il file da cui eseguire l'importazione 
-     * 
-     * 
-    */
-    public void importRubrica(String filename){
-
-    }
-
-    /**
-     * @brief Esegue l'esportazione della rubrica 
-     * 
-     * Esporta i contatti dell'arrayList in un file esterno specificato dall'utente
-     * 
-     * @param[in] filename indica il file su cui esportare la rubrica
-     * 
-    */
-
-    public void esportaRubrica(String filename){
-
     }
 
     
