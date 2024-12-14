0ad13cf8e0a35ceab55ce6e4e09b1c1f4a09d397
Simone <simonedonato37@gmail.com>
2024-12-14 15:24:52 +0000

    doxygen file

--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,24 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "acegiprs",
-  1: "aciprs",
-  2: "cr",
-  3: "acegiprs"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions"
 };
 
 var indexSectionLabels =
 {
-  0: "Tutto",
-  1: "Classi",
-  2: "File",
-  3: "Funzioni"
 };
 
